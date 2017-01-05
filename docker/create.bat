8eaa22e633caf1b592787af30e5f72f25134c993
Johan van Soest <johan.vansoest@maastro.nl>
2017-01-05 08:46:12 +0100

    Made batch script for building the generic shape expression image (jvsoest/shex:latest) and the thunder implementation (jvsoest/shex:thunder)

--- docker/create.bat
+++ docker/create.bat
@@ -1,1 +1,5 @@
 docker build -t jvsoest/shex:latest ./
+docker run -t --name shexTest -v c:/Users/johan.vansoest/Documents/Repositories/SAGE/ShapeExpressions/shapedir:/shape-import-dir -v c:/tmp/out:/output-dir jvsoest/shex:latest
+
+docker commit shexTest jvsoest/shex:thunder
+docker rm shexTest
